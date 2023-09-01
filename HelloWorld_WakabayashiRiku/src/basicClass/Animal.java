d49880968ca74c87839fb498c29fc3db42c68796
Biz038 <Biz038@DESKTOP-JDQ9HMT>
2023-09-01 16:55:59 +0900

    JAVA 19_1 修正

--- HelloWorld_WakabayashiRiku/src/basicClass/Animal.java
+++ HelloWorld_WakabayashiRiku/src/basicClass/Animal.java
@@ -1,5 +1,6 @@
 package basicClass;
 
+// フォーマットと、日付に関するインポート
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -7,15 +8,26 @@ public class Animal {
 
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
-		Dog dog = new Dog(10);
-		System.out.println(dog.name);
+
+		// ドッグのインスタンス作成
+		//引数の無い方
+		Dog dog1 = new Dog();
+		//  名前出力
+		System.out.println(dog1.name);
+
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
-		System.out.println(dog.numAnimal);
+		// 引数を持たせる
+		Dog dog2 = new Dog(10);
+		// 数を出力
+		System.out.println(dog2.numAnimal);
+
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
+
+		// カレンダーのインスタンス
 		Calendar calendar = Calendar.getInstance();
-		
+		// 出力形式の指定
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd H:m:s");
-		
+		// カレンダーから現在の時間を取得して、指定した形式を用いて出力
 		System.out.println(sdf.format(calendar.getTime()));
 	}
 
--- HelloWorld_WakabayashiRiku/src/basicClass/Dog.java
+++ HelloWorld_WakabayashiRiku/src/basicClass/Dog.java
@@ -2,16 +2,22 @@ package basicClass;
 
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
-	String name;
+	public String name;
 
 	// Q2：フィールドに動物の数の変数を定義してください。
-	int numAnimal;
+	public int numAnimal;
 
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
-	Dog(int num) {
+	// 引数なしのコンストラクタ
+	Dog() {
+		// 「犬」を代入
 		name = "犬";
+	}
 
-		// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	// 引数に整数を持つコンストラクタ
+	Dog(int num) {
+		// 引数を代入
 		numAnimal = num;
 	}
 }
